d7695d3348d54680b27969d10057a24e760ee13e
Alexis Villegas Torres <alexisvt@gmail.com>
2021-10-08 21:19:46 +0000

    Comment invalid line

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     const STARTING_MISSILES: i32 = 8;
     const READY_AMOUNT: i32 = 2;
 
-    READY_AMOUNT = 3;
+    // READY_AMOUNT = 3;
 
     // let mut missiles: i32 = STARTING_MISSILES;
     // let ready: i32 = READY_AMOUNT;
