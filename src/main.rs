91da4ad5ab598ca3a03ac822bc7961faad4239e8
avillegas <avillegas@tradestation.com>
2021-10-08 21:11:56 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    const STARTING_MISSILES: i32 = 8;
+    const READY_AMOUNT: i32 = 2;
+
+    READY_AMOUNT = 3;
+
+    // let mut missiles: i32 = STARTING_MISSILES;
+    // let ready: i32 = READY_AMOUNT;
+    let (missiles, ready) = (STARTING_MISSILES, READY_AMOUNT);
+    let _un_used_var: i32;
+
+    println!("Firing {} of my {} missiles...", ready, missiles);
+
+    println!("{} missiles left", missiles - ready);
+}
